53c318c1d20d9e07f0445bf112b71d438ee3ad9a
Herr Frei <herr.frei@googlemail.com>
2020-12-07 11:06:17 +0100

    Settings for Micro-Swiss Direct Drive Extruder with All Metal Hotend added

--- skr-v1.3--tmc2209-bltouch/marlin-2.0.7.2/Configuration.h
+++ skr-v1.3--tmc2209-bltouch/marlin-2.0.7.2/Configuration.h
@@ -70,7 +70,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Ender-5 SKR v1.3+TMC2209+BLTouch" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Ender-5 SKR v1.3+TMC2209+BLTouch+MicroSwiss)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -741,14 +741,14 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 93 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 130 } // 93: default extruder, 130: MicroSwiss extruder
 
 /**
  * Default Max Feed Rate (mm/s)
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 500, 500, 5, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 500, 500, 5, 35 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
